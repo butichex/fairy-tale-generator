fb41499184950832960b9b5f5a584631326d0c15
Тимур Дютин <tdyutin@gmail.com>
2023-09-10 11:08:05 +0300

    firijfr

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import "../src/reset.css"
 
 import { Configuration, OpenAI } from "openai"
 const openai = new OpenAI({
-    apiKey: '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',  dangerouslyAllowBrowser: true // defaults to process.env["OPENAI_API_KEY"]
+    apiKey: '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',  dangerouslyAllowBrowser: true, 
+    organization: "org-ytvh8R7rGVn52mnqZjlj1h6I" // defaults to process.env["OPENAI_API_KEY"]
 });
 createApp(App).use(router).mount('#app')
 
